46b15e1e1858fc0313516160de474703d2244373
Keshav Lawande <keshav.lawande@presentsoft.co.in>
2017-04-02 16:35:03 +0530

    Serializer Fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :sales_head
 
   def sales_head
-    sales_head = User.find(object.sales_head_id)
-    return sales_head.email
+    sales_head = object.sales_head.try(:email)
   end
 end
