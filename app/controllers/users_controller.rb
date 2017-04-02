937a1ac1f1919662529dadafd0485725535aa381
Keshav Lawande <keshav.lawande@presentsoft.co.in>
2017-04-02 15:09:29 +0530

    CRUD Operations on User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    if @users
+      render json: @users
+    else
+      render json: { errors: @users.errors }, status: 422
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: 201
+    else
+      render json: { errors: @user.errors }, status: 422
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: user, status: 201
+    else
+      render json: { errors: @user.errors}, status: 422
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render json: @user, status: 201
+    else
+      render json: { errors: @user.errors }, status: 422
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    head 204
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
